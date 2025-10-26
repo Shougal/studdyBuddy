7555d4b1be470caa3979d03fc9f31433a0872827
Shoug Alharbi <xdq9qa@virginia.edu>
2025-10-26 17:48:35 -0400

    feat(db setup): connect to database using PDO fix: PDO sql syntax fix
    * feat(setup): initial file setup following MVC
    
    * chore(setup): added full git ignore
    
    * feat(db setup): connect to database using PDO
    
    * chore(tables): file to run the initial table creations
    
    * fix: PDO sql syntax fix
    
    * refactor: removed dropping tables functionality to not lose data

---
+++ config/connect-db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$port = '3307'; // local host XAMPP port. P.S: you might need to change to default 3306, since I changed mine to 3307
+$dbname = 'studdyBuddy';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully to the database!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
