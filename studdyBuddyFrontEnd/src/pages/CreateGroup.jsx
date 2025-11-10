c781db2daada155fd6e25594ba86db0991b46231
shougal <xdq9qa@virginia.edu>
2025-11-09 20:06:50 -0500

    refactor

--- studdyBuddyFrontEnd/src/pages/CreateGroup.jsx
+++ studdyBuddyFrontEnd/src/pages/CreateGroup.jsx
@@ -4,29 +4,8 @@ import CreateGroupForm from "../components/forms/CreateGroupForm";
 export const CreateGroup = () => {
   const [error, setError] = useState("");
 
-  function parseTime12ToDate(timeStr) {
-    if (!timeStr) return null;
-
-    const [time, modifier] = timeStr.split(" "); // e.g. ["09:00", "PM"]
-    let [hours, minutes] = time.split(":");
-
-    hours = parseInt(hours, 10);
-    minutes = parseInt(minutes, 10);
-
-    if (modifier.toUpperCase() === "PM" && hours !== 12) {
-      hours += 12;
-    }
-    if (modifier.toUpperCase() === "AM" && hours === 12) {
-      hours = 0;
-    }
-
-    return new Date(1970, 0, 1, hours, minutes, 0);
-  }
-
   const checkTime = (form) => {
     const { start_time, end_time } = form;
-    // const start = parseTime12ToDate(start_time);
-    // const end = parseTime12ToDate(end_time);
 
     if (!start_time || !end_time) {
       alert("Please select both start and end times.");
