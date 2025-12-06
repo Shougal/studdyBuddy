5b4e86cc5a3ec5230a15a683708348a87f058d9f
shougal <xdq9qa@virginia.edu>
2025-12-06 11:35:03 -0500

    feat: allowing port 5174 for CORS settings

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // CORS settings for React dev server
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: http://localhost:5174");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");
