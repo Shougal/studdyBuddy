1d0628aff3e4b17b48306b92bbed1aa278e99ecd
shougal <xdq9qa@virginia.edu>
2025-12-05 20:22:51 -0500

    refactor code

--- api/handlers/users/create.php
+++ api/handlers/users/create.php
@@ -1,8 +1,8 @@
 <?php
 // api/handlers/users/create.php
 
-require_once __DIR__ . '/../../config/connect-db.php';
-require_once __DIR__ . '/../../queries/get_users.php';
+require_once __DIR__ . '/../../../config/connect-db.php';
+require_once __DIR__ . '/../../../queries/get_users.php';
 
 header('Content-Type: application/json');
 
