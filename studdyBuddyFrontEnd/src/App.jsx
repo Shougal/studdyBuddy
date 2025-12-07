0658d291000d496da7804a4858bca5dd020dc899
Alishaba Hayat <alishabahayat@gmail.com>
2025-12-07 17:38:34 -0500

    added comments to app.jsx

--- studdyBuddyFrontEnd/src/App.jsx
+++ studdyBuddyFrontEnd/src/App.jsx
@@ -1,38 +1,37 @@
 //app.jsx
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
+import React, { useState } from "react"; //stores logged in user
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  //import routing tools from react-router-dom to define different URLS for diff pages
+import Navbar from "./components/layout/Navbar"; //import navbar and all components that represent each screen in the app
 
-// Pages
+// Pages -import all pages into app
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
-import User from "./pages/User";
 import { CreateGroup } from "./pages/CreateGroup";
 import FindGroups from "./pages/FindGroups";
 import UserSchedule from "./pages/UserSchedule";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
+import User from "./pages/User";
 
-function App() {
-  const [user, setUser] = useState(null);
+function App() { //defines main app component
+  const [user, setUser] = useState(null); //creates state(user) that is null to begin with meaning no user;tracks who is logged in 
 
-  const handleLogin = (userData) => {
-    setUser(userData);
+  const handleLogin = (userData) => {  //called when login succeeds
+    setUser(userData); 
   };
 
-  const handleLogout = () => {
+  const handleLogout = () => { //clears user by setting user back to null
     setUser(null);
   };
 
   return (
-    <Router>
-      <Navbar onLogout={handleLogout} user={user} />
+    <Router> //wraps entire app in BrowserRouter, enabling URL based navigation
+      <Navbar onLogout={handleLogout} user={user} /> //nav bar is rendered on easch page
 
       <div className="page-container">
         <Routes>
           {/* Landing page if logged out, Home if logged in */}
-          <Route path="/" element={user ? <Home /> : <Landing />} />
-
+          <Route path="/" element={user ? <Home /> : <Landing />} /> //if user exists go to home if not landing
           <Route path="/groups" element={<FindGroups />} />
           <Route path="/schedule" element={<UserSchedule />} />
           <Route path="/create-group" element={<CreateGroup />} />
