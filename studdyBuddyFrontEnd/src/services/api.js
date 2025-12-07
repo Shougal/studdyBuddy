28b3ff52cae4fa8066b8cf0093d10ed89388c6dc
tt226 <tt262767@gmail.com>
2025-12-07 05:45:02 -0500

    removed marker

--- studdyBuddyFrontEnd/src/services/api.js
+++ studdyBuddyFrontEnd/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost/studdybuddy/api",
-=======
   baseURL: "http://localhost/studdybuddy/api", // base url before any endpoint call
->>>>>>> main
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
